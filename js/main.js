e2ea6e691ec50910b05c24364f79b73408bb001d
nikita <nikcfc@gmail.com>
2019-10-14 15:07:51 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('.c-select').SumoSelect( {
+    forceCustomRendering: false
+});
+$('.slick-vertical').slick({
+    vertical: true,
+    verticalSwiping: true,
+    slidesToShow: 3,
+    autoplay: false,
+});
+
+$('.slick-product-card').slick({
+    slidesToShow: 1,
+    autoplay: false,
+});
+
+$(".owl-carousel").owlCarousel({
+    loop:true,
+    nav:true,
+    items: 4,
+    mouseDrag:false,
+    pullDrag:false
+});
